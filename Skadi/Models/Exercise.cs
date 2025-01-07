e6502d7ea7061352c1a01a519116dc5efcb49cd0
Avngarde <pivotguy888@gmail.com>
2025-01-07 18:12:56 +0100

    Added Duration to Exercise model

--- Skadi/Models/Exercise.cs
+++ Skadi/Models/Exercise.cs
@@ -5,6 +5,7 @@ public class Exercise
     public int Id { get; set; }
     public string? ExerciseName { get; set; }
     public int Repetitions { get; set; }
+    public int Duration { get; set; } // To be used in place of repetitions if needed
     public Workout Workout { get; set; }
     public ExerciseType ExerciseType { get; set; }
 }
