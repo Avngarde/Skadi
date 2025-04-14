fd2430d67fe7737645ca640631ad52e1acdfb172
Avngarde <pivotguy888@gmail.com>
2025-04-14 14:22:45 +0200

    Add Break model

---
+++ Skadi/Models/Break.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skadi.Models
+{
+    public class Break
+    {
+        public int Minutes { get; set; }
+        public int Seconds { get; set; }
+    }
+}
