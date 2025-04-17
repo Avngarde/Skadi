d3835bec9294c5bb72af5dda1097f127b9f5f03c
Avngarde <pivotguy888@gmail.com>
2025-04-17 14:05:52 +0200

    Change CircularProgressBar animation length

--- Skadi/Custom/CircularProgressBar.xaml.cs
+++ Skadi/Custom/CircularProgressBar.xaml.cs
@@ -86,7 +86,8 @@ public partial class CircularProgressBar : ContentView
         animation.Commit(
             owner: this,
             name: "ProgressBarUpdateAnimation",
-            length: 350
+            length: 250
+
         );
     }
 
